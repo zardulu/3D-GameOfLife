8caca6823b342cfb7c902e01fed7d5e81a0ffecf
Anshul <61840818+zardulu@users.noreply.github.com>
2023-09-19 20:15:51 +0530

    CSS fixes

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// Create the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 100);
-
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
+
+// Responsive design adjustments
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
 
 const gridSize = 3;
 const cubeSize = 1;
@@ -21,19 +30,20 @@ function createCubes() {
     for (let j = 0; j < gridSize; j++) {
       for (let k = 0; k < gridSize; k++) {
         const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-        const material = new THREE.MeshPhongMaterial( {
-					color: 0x031cfc,
-					shininess: 50,
-					side: THREE.DoubleSide});
-          
+        const material = new THREE.MeshPhongMaterial({
+          color: 0x031cfc,
+          shininess: 50,
+          side: THREE.DoubleSide
+        });
+
         const cube = new THREE.Mesh(geometry, material);
 
         const posX = (cubeSize + gap) * (i - cubeSize);
-        const posY = (cubeSize + gap) * (j - cubeSize); 
+        const posY = (cubeSize + gap) * (j - cubeSize);
         const posZ = (cubeSize + gap) * (k - cubeSize);
 
         cube.position.set(posX, posY, posZ);
-        cube.castShadow = true; 
+        cube.castShadow = true;
         cubesGroup.add(cube);
       }
     }
@@ -56,12 +66,10 @@ spotLight.penumbra = 0.8;
 spotLight.position.set(5, 10, 0);
 spotLight.castShadow = true;
 spotLight.shadow.camera.near = 0.5;
-spotLight.shadow.camera.far = 20; 
+spotLight.shadow.camera.far = 20;
 spotLight.shadow.mapSize.width = 1024;
 spotLight.shadow.mapSize.height = 1024;
 scene.add(spotLight);
-
-
 
 // Ground
 const ground = new THREE.Mesh(
@@ -69,10 +77,11 @@ const ground = new THREE.Mesh(
   new THREE.MeshPhongMaterial({ color: 0xa0adaf, shininess: 200 })
 );
 ground.rotation.x = -Math.PI / 2;
-ground.position.y = -4; 
+ground.position.y = -4;
 ground.receiveShadow = true;
 scene.add(ground);
 
+// Orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 1, 0);
 controls.update();
@@ -102,6 +111,10 @@ function animate() {
 }
 
 // Event listeners
-document.addEventListener("wheel", onDocumentMouseScroll, false);
+document.addEventListener('wheel', onDocumentMouseScroll, false);
 
+// Initial call to resize the canvas
+onWindowResize();
+
+// Start the animation loop
 animate();
